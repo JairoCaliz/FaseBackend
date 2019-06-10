3165d90dcd03a98c02ccef0a4f1fffae86c2a703
JairoCaliz <50463413+JairoCaliz@users.noreply.github.com>
2019-06-10 14:15:31 -0500

    Add files via upload

---
+++ bin/Controller.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const User = require("./models/User");
+
+class Controller{
+    constructor(){
+        this.connect();
+    }
+
+   async connect(){
+       try{
+        await mongoose.connect(
+            "mongodb+srv://jcalizhoyos:1069495296caliz@cluster0-ul61k.mongodb.net/projectDB?retryWrites=true&w=majority",
+            {userNewUrlParser:true}
+        );
+        console.log("Conectados a la base de datos")
+
+       } catch(e){
+           console.error(e)
+       }
+   }
+
+   getUsers(res){
+       User.find({}, (err, users)=>{
+           if(err) throw err;
+
+           res.send(users);
+       })
+   }
+   postUsers(req, res){
+       let user = req.body.users;
+       User.create(user, (err, newUser)=>{
+             if(err) throw err;
+             res.send({nU:newUser})
+       })
+   }
+
+}
+
+exports.controller = new Controller()
---
+++ bin/routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const {controller} = require("./Controller");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res )=>{
+    res.send("Hola Mundo!");
+})
+
+app.get("/users", (req, res)=>{
+    controller.getUsers(res);
+})
+app.post("/users", (req, res)=>{
+    controller.postUsers(req, res)
+})
+
+exports.app = app;
